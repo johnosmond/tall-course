07e5f7f964d213a1ede1731759a8a62d8ba29fcc
John Osmond <john@jtech10.com>
2024-06-19 14:39:41 -0400

    adds modal component

--- resources/views/livewire/landing-page.blade.php
+++ resources/views/livewire/landing-page.blade.php
@@ -1,6 +1,7 @@
-<div x-data="{ showSubscribe: false }" class="flex flex-col bg-indigo-900 w-full h-screen">
+<div class="flex flex-col bg-indigo-900 w-full h-screen" x-data="{ showSubscribe: false, showMessage: false, }">
+
     <nav class="flex pt-5 justify-between container mx-auto text-indigo-300">
-        <a href="{{ route('home') }}" class="text-4xl font-bold">
+        <a class="text-4xl font-bold" href="{{ route('home') }}">
             <x-application-logo class="w-16 h-auto fill-current" />
         </a>
         <div class="flex justify-end">
@@ -27,19 +28,32 @@
         </div>
     </div>
 
-    <div x-cloak x-show="showSubscribe" x-on:click.self="showSubscribe=false" x-on:keydown.escape.window="showSubscribe=false" class="flex fixed top-0 bg-gray-900 bg-opacity-60 items-center w-full h-full">
-        <div class="bg-pink-500 m-auto shadow-2xl rounded-xl p-8">
-            <p class="text-white text-5xl font-extrabold text-center">Let's do it!</p>
-            <form class="flex flex-col items-center p-24 gap-4" wire:submit.prevent="subscribe">
-                <x-text-input class="px-5 py-3 w-80 border border-blue-400" name="email" type="email"
-                    placeholder="Email" wire:model="email" />
-                <span class="text-gray-100 text-sm">
-                    We will send you a confirmation email.
-                </span>
-                <x-primary-button class="w-80">
-                    <span class="mx-auto">Get In!</span>
-                </x-primary-button>
-            </form>
-        </div>
-    </div>
+    {{-- modal form --}}
+    <x-modal1 class="bg-pink-500" trigger="showSubscribe">
+        <p class="text-white text-5xl font-extrabold text-center">Let's do it!</p>
+        <form class="flex flex-col items-center p-24 gap-4" wire:submit.prevent="subscribe">
+            <x-text-input class="px-5 py-3 w-80 border border-blue-400" name="email" type="email"
+                placeholder="Email" wire:model="email" />
+            <span class="text-gray-100 text-sm">
+                We will send you a confirmation email.
+            </span>
+            <x-primary-button class="w-80">
+                <span class="mx-auto">Get In!</span>
+            </x-primary-button>
+        </form>
+    </x-modal1>
+
+    {{-- modal message --}}
+    <x-modal1 class="bg-green-500" trigger="showMessage">
+        <p class="animate-pulse text-white text-9xl font-extrabold text-center">
+            &check;
+        </p>
+        <p class="text-white text-5xl font-extrabold text-center mt-10">
+            Great!
+        </p>
+        <p class="text-white text-3xl text-center">
+            See you in your inbox.
+        </p>
+    </x-modal1>
+
 </div>
